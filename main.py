e4299a8c317c384eca3ebeb7cf17d9e536c7930f
krutovsky-danya <krutovsky.danya@gmail.com>
2023-12-02 15:37:06 +0500

    format main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from pipeline.study_agent import study_dqn
 from algorithms.deep_q_networks import DQN
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     agent = DQN(1, 1)
 
     rewards, estimates = study_dqn(None, agent, 0)
