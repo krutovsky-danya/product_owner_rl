31e4593e44fcf7df2ef7a41b4f0705405d28f8ef
Catharine <ekaterina.vasilko@mail.ru>
2024-02-20 14:24:16 +0500

    update base study

--- pipeline/base_study.py
+++ pipeline/base_study.py
@@ -1,5 +1,5 @@
+from environment import ProductOwnerEnv
 from algorithms.deep_q_networks import DQN
-from environment.environment import ProductOwnerEnv
 
 MAX_INNER_SPRINT_ACTION_COUNT = 20
 
@@ -16,8 +16,13 @@ class BaseStudy:
     def play_trajectory(self, init_state):
         total_reward = 0
         state = init_state
-        for t in range(self.trajectory_max_len):
-            action = self.agent.get_action(state)
+        t = 0
+        inner_sprint_action_count = 0
+        done = False
+
+        while not done:
+            action, inner_sprint_action_count = self._choose_action(state,
+                                                                    inner_sprint_action_count)
             next_state, reward, done, _ = self.env.step(action)
 
             self.fit_agent(state, action, reward, done, next_state)
@@ -25,10 +30,23 @@ class BaseStudy:
             state = next_state
             total_reward += reward
 
-            if done:
-                break
+            t += 1
+            done = (t == self.trajectory_max_len)
 
         return total_reward
+
+    def _choose_action(self, state, inner_sprint_action_count):
+        chosen_action = self.agent.get_action(state)
+        if chosen_action == 0:
+            inner_sprint_action_count = 0
+        else:
+            inner_sprint_action_count += 1
+        if inner_sprint_action_count > MAX_INNER_SPRINT_ACTION_COUNT:
+            chosen_action = 0
+            inner_sprint_action_count = 0
+            if not self.env.IS_SILENT:
+                print("enforced next sprint")
+        return chosen_action, inner_sprint_action_count
 
     def study_agent(self, episode_n: int):
         for episode in range(episode_n):
