883d100bbbfbcd6a7cb947bd5f76bf015fa6d917
Catharine <ekaterina.vasilko@mail.ru>
2024-04-19 21:26:14 +0500

    update aggregator study

--- pipeline/aggregator_study.py
+++ pipeline/aggregator_study.py
@@ -1,6 +1,6 @@
 from environment.userstory_env import UserstoryEnv
 from environment.backlog_env import BacklogEnv
-from environment.reward_sytem import BaseRewardSystem, EmpiricalRewardSystem, EmpiricalCreditStageRewardSystem
+from environment.reward_sytem import BaseRewardSystem, EmpiricalRewardSystem, EmpiricalCreditStageRewardSystem, FullPotentialCreditRewardSystem
 from pipeline.logging_study import LoggingStudy
 from environment import TutorialSolverEnv, CreditPayerEnv, ProductOwnerEnv
 from environment.credit_payer_env import USUAL_CREDIT_ENV_END_SPRINT, EARLY_CREDIT_ENV_END_SPRINT
@@ -13,8 +13,8 @@ def update_reward_system_config(env: ProductOwnerEnv, reward_system: BaseRewardS
     reward_system.config["remove_sprint_card_actions"] = actions
 
 class AggregatorStudy(LoggingStudy):
-    def __init__(self, env, agents, trajectory_max_len, save_rate=100,
-                 backlog_environments=None) -> None:
+    def __init__(self, env, agents, trajectory_max_len, save_rate=None,
+                 backlog_environments=None, reward_systems=None) -> None:
         assert 0 < len(agents) < 5
         assert backlog_environments is None or len(backlog_environments) == len(agents) - 1
         self.stage = len(agents)
@@ -28,33 +28,48 @@ class AggregatorStudy(LoggingStudy):
             assert isinstance(env, ProductOwnerEnv)
 
         self.agents = agents
+        self.backlog_environments = backlog_environments
         if backlog_environments is None:
             self.backlog_environments = [None] * self.stage
+        self.rewards_systems = reward_systems
         super().__init__(env, agents[-1], trajectory_max_len, save_rate)
 
     def play_trajectory(self, state, info):
         full_reward = 0
         if self.stage > 1:
-            if self.backlog_environments[0] is None:
-                self.backlog_environments[0] = BacklogEnv(4, 0, 0, 0, 0, 0)
+            if self.rewards_systems is None or len(self.rewards_systems) == 0:
+                tutorial_reward_system = EmpiricalRewardSystem(config={})
+            else:
+                tutorial_reward_system = self.rewards_systems[0]
             state, info, reward, failed = self.play_tutorial(self.agents[0],
-                                                             self.backlog_environments[0])
+                                                             self.backlog_environments[0],
+                                                             tutorial_reward_system)
             full_reward += reward
             if failed:
                 self._log_trajectory_end(full_reward)
                 return reward
         if self.stage > 2:
+            if self.rewards_systems is None or len(self.rewards_systems) < 2:
+                credit_reward_system = FullPotentialCreditRewardSystem(config={})
+            else:
+                credit_reward_system = self.rewards_systems[1]
             state, info, credit_reward, failed = self.play_credit_payment(self.agents[1],
                                                                           self.backlog_environments[1],
-                                                                          False)
+                                                                          False,
+                                                                          credit_reward_system)
             full_reward += credit_reward
             if failed:
                 self._log_trajectory_end(full_reward)
                 return full_reward
         if self.stage > 3:
+            if self.rewards_systems is None or len(self.rewards_systems) < 3:
+                credit_reward_system = FullPotentialCreditRewardSystem(config={})
+            else:
+                credit_reward_system = self.rewards_systems[2]
             state, info,  credit_reward, failed = self.play_credit_payment(self.agents[2],
                                                                            self.backlog_environments[2],
-                                                                           True)
+                                                                           True,
+                                                                           credit_reward_system)
             full_reward += credit_reward
             if failed:
                 self._log_trajectory_end(full_reward)
@@ -63,16 +78,14 @@ class AggregatorStudy(LoggingStudy):
         self.logger.info(f"full total_reward: {full_reward}")
         return full_reward
 
-    def play_tutorial(self, tutorial_agent, tutorial_backlog_env):
-        reward_system = EmpiricalRewardSystem(config={})
-        env = TutorialSolverEnv(backlog_env=tutorial_backlog_env, with_info=self.env.with_info, reward_system=reward_system)
-        update_reward_system_config(env, reward_system)
+    def play_tutorial(self, tutorial_agent, tutorial_backlog_env, tutorial_reward_system):
+        env = TutorialSolverEnv(backlog_env=tutorial_backlog_env, with_info=self.env.with_info, reward_system=tutorial_reward_system)
+        update_reward_system_config(env, tutorial_reward_system)
         done = not self.env.game.context.is_new_game
 
         return self.play_some_stage(tutorial_agent, env, done, "tutorial")
 
-    def play_credit_payment(self, credit_agent, credit_backlog_env, with_end):
-        reward_system = EmpiricalCreditStageRewardSystem(with_late_purchase_punishment=True, config={})
+    def play_credit_payment(self, credit_agent, credit_backlog_env, with_end, reward_system):
         env = CreditPayerEnv(backlog_env=credit_backlog_env, with_end=with_end,
                              with_info=self.env.with_info, reward_system=reward_system)
         end_sprint = USUAL_CREDIT_ENV_END_SPRINT if with_end else EARLY_CREDIT_ENV_END_SPRINT
@@ -85,7 +98,7 @@ class AggregatorStudy(LoggingStudy):
         agent.epsilon = 0
         translator_env.game = self.env.game
         done = init_done
-        state = translator_env._get_state()
+        state = translator_env.recalculate_state()
         info = translator_env.get_info()
         inner_sprint_action_count = 0
         total_reward = 0
@@ -104,5 +117,5 @@ class AggregatorStudy(LoggingStudy):
         if translator_env.game.context.get_money() < 0:
             self.logger.debug(f"{name} failed")
 
-        return self.env._get_state(), self.env.get_info(), \
+        return self.env.recalculate_state(), self.env.get_info(), \
                total_reward, translator_env.game.context.get_money() < 0
