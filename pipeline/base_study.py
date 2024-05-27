a800c7ac633d5ecc1b7f511f43ed0241245481fd
Catharine <ekaterina.vasilko@mail.ru>
2024-05-27 14:26:10 +0500

    add discounted reward into base study

--- pipeline/base_study.py
+++ pipeline/base_study.py
@@ -14,8 +14,10 @@ class BaseStudy:
     def fit_agent(self, state, action, reward, done, next_state, next_info):
         return self.agent.fit(state, action, reward, done, next_state, next_info)
 
-    def play_trajectory(self, init_state, init_info):
+    def play_trajectory(self, init_state, init_info, init_discount=1):
         total_reward = 0
+        total_discounted_reward = 0
+        discount = init_discount
         state = init_state
         info = init_info
         t = 0
@@ -32,11 +34,13 @@ class BaseStudy:
 
             state = next_state
             total_reward += reward
+            total_discounted_reward += reward * discount
+            discount *= self.agent.gamma
 
             t += 1
             done = done or (t == self.trajectory_max_len)
 
-        return total_reward
+        return total_reward, total_discounted_reward
 
     def _choose_action(self, action, inner_sprint_action_count) -> Tuple[int, int]:
         if action == 0:
