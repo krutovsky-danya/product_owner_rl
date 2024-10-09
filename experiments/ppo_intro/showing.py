d89313ac26b42cc783a7d68c24da0e6da99e8a1f
krutovsky-danya <krutovsky.danya@gmail.com>
2024-10-09 13:00:35 +0500

    prepare showing

---
+++ experiments/ppo_intro/showing.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+def main():
+    sub_name = "100_episodes_15_trajectory_n"
+    rewards_df = pd.read_csv(f"train_rewards_{sub_name}.csv")
+    reward_groups = rewards_df.groupby(["Trajectory"])["Reward"]
+    mean_rewards = reward_groups.mean().reset_index()
+    default_rewards = mean_rewards
+
+    plt.plot(
+        default_rewards["Trajectory"], default_rewards["Reward"], ".", label="Default"
+    )
+    plt.legend()
+    plt.title("Rewards")
+    plt.xlabel("trajectory")
+    plt.ylabel("rewards")
+    plt.xticks(np.arange(0, 1501, 500))
+    plt.savefig(f"rewards_{sub_name}.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
