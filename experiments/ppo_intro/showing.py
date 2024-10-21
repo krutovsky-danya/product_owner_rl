7c2a3ff0b691d268998e427a476ee4afacd409e1
krutovskii <krutovskii@skbkontur.ru>
2024-10-21 19:20:59 +0500

    More data and showing

--- experiments/ppo_intro/showing.py
+++ experiments/ppo_intro/showing.py
@@ -3,19 +3,14 @@ import numpy as np
 import pandas as pd
 
 
-def main():
-    episode_n = 250
-    trajectory_n = 20
-    sub_name = f"CreditPayerEnv_{episode_n}_episodes_{trajectory_n}_trajectory_nn_2x256_inner_layer_gamma_0.9"
+def show_rewards_fitting(sub_name):
     rewards_df = pd.read_csv(f"train_rewards_{sub_name}.csv")
     reward_groups = rewards_df.groupby(["Trajectory", "ExperimentName"])["Reward"]
     mean_rewards = reward_groups.mean().reset_index()
 
-    for exp_name in set(rewards_df['ExperimentName']):
-        rewards = mean_rewards[mean_rewards['ExperimentName'] == exp_name]
-        plt.plot(
-        rewards["Trajectory"], rewards["Reward"], ".", label=exp_name
-    )
+    for exp_name in set(rewards_df["ExperimentName"]):
+        rewards = mean_rewards[mean_rewards["ExperimentName"] == exp_name]
+        plt.plot(rewards["Trajectory"], rewards["Reward"], ".", label=exp_name)
     plt.legend()
     plt.grid()
     plt.title("Rewards")
@@ -23,6 +18,26 @@ def main():
     plt.ylabel("rewards")
     plt.savefig(f"rewards_{sub_name}.png")
     plt.show()
+
+
+def show_win_rate(sub_name):
+    evals_df = pd.read_csv(f"evaluations_{sub_name}.csv")
+    wins = evals_df.groupby(["DateTime", "ExperimentName"])["Win"]
+    groups_wins = wins.sum()
+
+    print(groups_wins)
+
+    total_wins = groups_wins.reset_index()
+    total_wins = total_wins.groupby(["ExperimentName"])['Win'].sum()
+    print(total_wins)
+
+
+def main():
+    episode_n = 250
+    trajectory_n = 20
+    sub_name = f"CreditPayerEnv_{episode_n}_episodes_{trajectory_n}_trajectory_nn_2x256_inner_layer_gamma_0.9"
+    # show_rewards_fitting(sub_name)
+    show_win_rate(sub_name)
 
 
 if __name__ == "__main__":
