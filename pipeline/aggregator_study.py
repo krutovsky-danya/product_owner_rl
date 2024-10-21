4d293a047d13ab6d816fbc45a701f9f5ea5cd7d0
Catharine <ekaterina.vasilko@mail.ru>
2024-10-21 21:02:06 +0500

    refactor aggregator study

--- pipeline/aggregator_study.py
+++ pipeline/aggregator_study.py
@@ -3,7 +3,6 @@ from environment.reward_sytem import EmpiricalRewardSystem, EmpiricalCreditStage
 from environment.reward_sytem import FullPotentialCreditRewardSystem
 from pipeline.logging_study import LoggingStudy, KeyLogState
 from environment import TutorialSolverEnv, CreditPayerEnv, ProductOwnerEnv
-from environment.credit_payer_env import USUAL_CREDIT_ENV_END_SPRINT, EARLY_CREDIT_ENV_END_SPRINT
 
 from typing import Dict, Optional, List
 import logging
@@ -51,38 +50,47 @@ class AggregatorStudy(LoggingStudy):
                          save_memory, base_epoch_log_state, base_end_log_state, log_level=log_level)
 
     def play_trajectory(self, state, info, init_discount=1):
-        full_reward = 0
-        full_discounted_reward = 0
-        discount = init_discount
+        (full_reward, full_discounted_reward,
+         state, info, discount, failed) = self.play_stages_before_study(state, info, init_discount)
+        if failed:
+            self._log_trajectory_end(full_reward)
+            return full_reward, full_discounted_reward
 
-        for name in self.order:
-            translator_env = self.get_translator_env(name)
-            init_done = self.get_initial_done(name)
-            update_reward_system_config(translator_env, translator_env.reward_system)
-            state, info, reward, failed, discounted_reward, discount = self.play_some_stage(
-                self.agents[name],
-                translator_env,
-                init_done,
-                name,
-                discount
-            )
-            full_reward += reward
-            full_discounted_reward += discounted_reward
-            if failed:
-                self._log_trajectory_end(full_reward)
-                return full_reward, full_discounted_reward
         reward_study, discounted_reward_study = super().play_trajectory(state, info, discount)
         full_reward += reward_study
         full_discounted_reward += discounted_reward_study
         self.logger.info(f"full total_reward: {full_reward}")
         return full_reward, full_discounted_reward
 
-    def play_some_stage(self, agent, translator_env, init_done, name, init_discount):
+    def play_stages_before_study(self, state, info, init_discount):
+        full_reward = 0
+        full_discounted_reward = 0
+        discount = init_discount
+        failed = False
+
+        for name in self.order:
+            translator_env = self.get_translator_env(name)
+            update_reward_system_config(translator_env, translator_env.reward_system)
+            state, info, reward, failed, discounted_reward, discount = self.play_some_stage(
+                self.agents[name],
+                translator_env,
+                name,
+                discount
+            )
+            full_reward += reward
+            full_discounted_reward += discounted_reward
+            if failed:
+                break
+        return (full_reward, full_discounted_reward,
+                state, info, discount, failed)
+
+    def play_some_stage(self, agent, translator_env, name, init_discount):
         agent.eval()
         translator_env.game = self.env.game
-        done = init_done
+        translator_env.card_picker_random_generator = self.env.card_picker_random_generator
         state = translator_env.recalculate_state()
         info = translator_env.get_info()
+        done = translator_env.get_done(info)
         inner_sprint_action_count = 0
         total_reward = 0
         total_discounted_reward = 0
@@ -106,7 +114,10 @@ class AggregatorStudy(LoggingStudy):
 
         state = self.env.recalculate_state()
         info = self.env.get_info()
-        failed = translator_env.game.context.get_money() < 0
+        context = translator_env.game.context
+        lost_money = context.get_money() < 0
+        lost_customers = not context.is_new_game and context.customers <= 0
+        failed = lost_money or lost_customers
 
         return state, info, total_reward, failed, total_discounted_reward, discount
 
@@ -167,15 +178,3 @@ class AggregatorStudy(LoggingStudy):
             return EmpiricalCreditStageRewardSystem(with_late_purchase_punishment=True, config={})
 
         return EmpiricalRewardSystem(config={})
-
-    def get_initial_done(self, name):
-        info = self.env.get_info()
-        game_ended = self.env.get_done(info)
-        if game_ended:
-            return game_ended
-        if name == TUTORIAL:
-            return not self.env.game.context.is_new_game
-        if name == CREDIT_FULL or name == CREDIT_END:
-            return self.env.game.context.current_sprint >= USUAL_CREDIT_ENV_END_SPRINT
-        if name == CREDIT_START:
-            return self.env.game.context.current_sprint >= EARLY_CREDIT_ENV_END_SPRINT
