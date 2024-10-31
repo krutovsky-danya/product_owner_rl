333148b0cdad51ba021e39e4646fda9c27380eee
krutovsky-danya <krutovsky.danya@gmail.com>
2024-10-31 22:29:26 +0500

    Считываение всей user story

--- tests/test_game_state_parsing.py
+++ tests/test_game_state_parsing.py
@@ -22,13 +22,17 @@ class TestInitialGameParsing:
     expected_board_path = image_directory + "/expected_user_story_board.png"
     _expected_board = cv2.imread(expected_board_path)
 
-    expected_row_path = image_directory + "/initial_user_story.png"
-    _expected_row = cv2.imread(expected_row_path)
+    expected_user_story_path = image_directory + "/initial_user_story.png"
+    _expected_user_story = cv2.imread(expected_user_story_path)
 
-    expected_user_story_loyalty_path = image_directory + '/expected_user_story_loyalty.png'
+    expected_user_story_loyalty_path = (
+        image_directory + "/expected_user_story_loyalty.png"
+    )
     _expected_user_story_loyalty = cv2.imread(expected_user_story_loyalty_path)
 
-    expecrted_user_story_customers_path = image_directory + '/expected_user_story_users.png'
+    expecrted_user_story_customers_path = (
+        image_directory + "/expected_user_story_users.png"
+    )
     _expected_user_story_customers = cv2.imread(expecrted_user_story_customers_path)
 
     def setup_method(self):
@@ -56,7 +60,7 @@ class TestInitialGameParsing:
         # arrange
         original_shape = (1028, 1920, 3)
         board = self._expected_board.copy()
-        expected_row = self._expected_row.copy()
+        expected_row = self._expected_user_story.copy()
 
         # act
         rows = self.image_parser.get_rows(board, original_shape)
@@ -68,14 +72,16 @@ class TestInitialGameParsing:
         assert position == (1557, 384)
         images_diff = cv2.absdiff(row, expected_row)
         assert np.all(images_diff == 0)
-    
+
     def test_image_parser_selects_user_strory_loyalty(self):
         # arrange
-        user_stoty_image = self._expected_row.copy()
+        user_stoty_image = self._expected_user_story.copy()
         expected_loyalty_line = self._expected_user_story_loyalty.copy()
 
         # act
-        actual_loyalty = self.image_parser.get_user_story_loaylty_image(user_stoty_image, self.original_shape)
+        actual_loyalty = self.image_parser.get_user_story_loaylty_image(
+            user_stoty_image, self.original_shape
+        )
 
         # assert
         assert actual_loyalty.shape == expected_loyalty_line.shape
@@ -90,20 +96,22 @@ class TestInitialGameParsing:
         line = self.image_parser.read_line(loyalty_line_image, 11)
 
         # assert
-        assert line == '+0.045'
-    
+        assert line == "+0.045"
+
     def test_select_user_story_users(self):
         # arrange
-        user_stroy_image = self._expected_row.copy()
+        user_stroy_image = self._expected_user_story.copy()
         expected_users_line = self._expected_user_story_customers.copy()
 
         # act
-        actual_users = self.image_parser.get_user_story_users_image(user_stroy_image, self.original_shape)
+        actual_users = self.image_parser.get_user_story_users_image(
+            user_stroy_image, self.original_shape
+        )
 
         assert actual_users.shape == expected_users_line.shape
         image_diff = cv2.absdiff(actual_users, expected_users_line)
         assert np.all(image_diff == 0)
-    
+
     def test_read_user_story_customers(self):
         # arrange
         customers_line = self._expected_user_story_customers.copy()
@@ -112,7 +120,19 @@ class TestInitialGameParsing:
         line = self.image_parser.read_line(customers_line, 11)
 
         # assert
-        assert line == '+1000'
+        assert line == "+1000"
+
+    def test_read_user_story(self):
+        # arrange
+        user_story_image = self._expected_user_story.copy()
+
+        # act
+        user_story_info = self.image_parser.read_user_story(
+            user_story_image, self.original_shape
+        )
+
+        # assert
+        assert user_story_info == ((115, 188, 30), 0.045, 1.0)
 
     # def test_user_stories_parsing(self):
     #     web_interaction.insert_user_stories_from_image(self.game, self.initial_image)
