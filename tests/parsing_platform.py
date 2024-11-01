0549b6d81e15b981d87b9ff24723ea4feea6095c
krutovsky-danya <krutovsky.danya@gmail.com>
2024-11-01 23:26:00 +0500

    Добавил цвет

--- tests/parsing_platform.py
+++ tests/parsing_platform.py
@@ -8,6 +8,8 @@ class ParsingPlatform:
     image_directory = "tests/test_images"
     original_shape = (1028, 1920, 3)
 
+    orange = (43, 194, 249)
+
     def read_game_start(self, id):
         image_path = self.image_directory + f"/game_start_{id}.png"
         return cv2.imread(image_path)
