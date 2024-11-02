a0cb08fd19b45835af450a507e21b49388bc373c
krutovsky-danya <krutovsky.danya@gmail.com>
2024-11-03 00:59:49 +0500

    Улучшил наследование

--- web_interaction/single_color_storage.py
+++ web_interaction/single_color_storage.py
@@ -1,8 +1,13 @@
 from game.game_colors import ColorStorage
 
+
 class SingleColorStorage(ColorStorage):
     def __init__(self, color):
+        super().__init__()
         self.color = color
-    
+
     def get_unused_color(self, uc_type, random):
         return self.color
+
+    def release_color(self, us_type, color):
+        pass
