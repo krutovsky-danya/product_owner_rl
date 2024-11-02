e2b7d46cef526d186f26502ab0b1f2de107fd1e3
krutovskii <krutovskii@skbkontur.ru>
2024-11-02 16:17:13 +0500

    Добавил поиск карточек беклога

--- web_interaction/backlog_card_image_info.py
+++ web_interaction/backlog_card_image_info.py
@@ -1,10 +1,20 @@
+from game.backlog_card.card_info import CardInfo
+
+
 class BacklogCardImageInfo:
     def __init__(self, color, hours, position):
         self.color = color
         self.hours = hours
         self.position = position
 
+    def _equals_backlog_card(self, value: CardInfo):
+        if self.color != value.color:
+            return False
+        return self.hours == value.base_hours
+
     def __eq__(self, value: object) -> bool:
+        if isinstance(value, CardInfo):
+            return self._equals_backlog_card(value)
         if not isinstance(value, BacklogCardImageInfo):
             return False
         if self.color != value.color:
