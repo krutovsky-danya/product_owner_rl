f4887b64a1987d243f45b768dd451203295b6d12
krutovskii <krutovskii@skbkontur.ru>
2025-01-30 17:05:50 +0500

    Поправил построение графиков

--- experiments/show_utils.py
+++ experiments/show_utils.py
@@ -9,7 +9,8 @@ def moving_average(x, w):
     return np.convolve(x, np.ones(w), "full")[: -w + 1] / w
 
 
-def show_rewards_fitting(df: pd.DataFrame, columns: List):
+def show_rewards_fitting(df: pd.DataFrame):
+    columns = ["Reward"]
     reward_groups = df.groupby(["Trajectory", "ExperimentName"])[columns]
     mean_rewards = reward_groups.mean().reset_index()
 
@@ -26,7 +27,29 @@ def show_rewards_fitting(df: pd.DataFrame, columns: List):
     plt.title("Rewards")
     plt.xlabel("trajectory")
     plt.ylabel("rewards")
-    plt.savefig("_".join(columns) + ".png")
+    plt.savefig("Rewards.png")
+    plt.show()
+
+
+def show_estimate_reward_comparison(df: pd.DataFrame, agent_name: str):
+    columns = ["Estimate", "DiscountedReward"]
+    data = df[df["ExperimentName"] == agent_name]
+    data = data.groupby(["Trajectory"])[columns]
+    mean_data = data.mean().reset_index()
+
+    for column in columns:
+        plt.plot(
+            mean_data["Trajectory"],
+            moving_average(mean_data[column], 5),
+            label=column,
+        )
+
+    plt.legend()
+    plt.grid()
+    plt.title(agent_name)
+    plt.xlabel("trajectory")
+    plt.ylabel("rewards")
+    plt.savefig(f"Esitmates-Rewards-{agent_name}.png")
     plt.show()
 
 
