7a5c6e957f655a464f305d016b2ca9e99e09331e
krutovsky-danya <krutovsky.danya@gmail.com>
2025-02-09 13:40:39 +0500

    Добавил создание обычной среды

--- environment/environments_factory.py
+++ environment/environments_factory.py
@@ -1,4 +1,4 @@
-from environment import CreditPayerEnv
+from environment import CreditPayerEnv, ProductOwnerEnv
 from environment.backlog_env import BacklogEnv
 from environment.userstory_env import UserstoryEnv
 from environment.reward_sytem import FullPotentialCreditRewardSystem
@@ -15,6 +15,21 @@ class EnvironmentFactory:
             userstory_env,
             backlog_env,
             with_end=True,
+            with_info=True,
+            reward_system=reward_system,
+        )
+        update_reward_system_config(env, reward_system)
+
+        return env
+
+    def create_full_env(self):
+        userstory_env = UserstoryEnv(4, 2, 2)
+        backlog_env = BacklogEnv(12, 6, 6, 0, 0, 0)
+        reward_system = FullPotentialCreditRewardSystem(config={}, coefficient=1)
+
+        env = ProductOwnerEnv(
+            userstory_env,
+            backlog_env,
             with_info=True,
             reward_system=reward_system,
         )
