db777643ebe2350254c4ee0745ee283e7f241fef
krutovsky-danya <krutovsky.danya@gmail.com>
2025-02-15 22:37:43 +0300

    Добавил readme, данные, отрисовку и новый подсчет статистической значимости

--- algorithms/deep_q_networks.py
+++ algorithms/deep_q_networks.py
@@ -78,6 +78,8 @@ class DQN(nn.Module):
                          dim=1).values
 
     def get_padded_to_action_n(self, guide: torch.Tensor):
+        if len(guide) == 0:
+            return torch.zeros(self.q_function.action_n)
         return torch.nn.functional.pad(guide,
                                        pad=(0, self.q_function.action_n - guide.numel()),
                                        mode="constant",
