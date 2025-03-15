2492c9f929614815cf0ac2009d78c61fb63adcf0
krutovsky-danya <krutovsky.danya@gmail.com>
2025-03-15 16:26:10 +0300

    Вынес Q-функцию в отдельный файл

--- algorithms/deep_q_networks.py
+++ algorithms/deep_q_networks.py
@@ -4,36 +4,7 @@ import numpy as np
 
 import random
 
-
-class QFunction(nn.Module):
-    def __init__(self, state_dim, action_n, inner_layer=512):
-        super().__init__()
-        self.state_dim = state_dim
-        self.action_n = action_n
-        self.inner_layer = inner_layer
-
-        self.network = nn.Sequential(
-            # nn.BatchNorm1d(state_dim),
-            nn.Linear(state_dim, self.inner_layer),
-            nn.ReLU(),
-            # nn.BatchNorm1d(self.inner_layer),
-            nn.Linear(self.inner_layer, self.inner_layer),
-            nn.ReLU(),
-            # nn.BatchNorm1d(self.inner_layer),
-            nn.Linear(self.inner_layer, self.inner_layer),
-            nn.ReLU(),
-            # nn.BatchNorm1d(self.inner_layer),
-            nn.Linear(self.inner_layer, action_n),
-        )
-
-    def forward(self, states):
-        return self.network(states)
-
-    def predict(self, state):
-        self.network.eval()
-        result = self.network(state.unsqueeze(0))
-        self.network.train(True)
-        return result
+from .q_function import QFunction
 
 
 class DQN(nn.Module):
@@ -51,7 +22,7 @@ class DQN(nn.Module):
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.q_function = QFunction(self.state_dim, self.action_dim).to(device=self.device)
+        self.q_function = QFunction(state_dim, action_dim).to(device=self.device)
         self.gamma = gamma
         self.batch_size = batch_size
         self.epsilon = 1
@@ -152,14 +123,7 @@ class TargetDQN(DQN):
         super().__init__(
             state_dim, action_dim, gamma, lr, batch_size, epsilon_decrease, epsilon_min
         )
-        self.target_q_function = QFunction(state_dim, action_dim).to(self.device)
-
-        state_dict = self.q_function.network.state_dict()
-        self.target_q_function.network.load_state_dict(state_dict)
-
-        for p in self.target_q_function.network.parameters():
-            p.requires_grad = False
-
+        self.target_q_function = self.q_function.get_target_copy(self.device)
         self.target_update = target_update
         self.fit_calls = 0
 
