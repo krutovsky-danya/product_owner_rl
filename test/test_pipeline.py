925abccac1a57889038ac26f7c50b9b376f2c333
krutovsky-danya <krutovsky.danya@gmail.com>
2025-03-23 20:17:24 +0300

    Поправил тесты

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -1,13 +1,12 @@
+from algorithms.agents_factory import DqnAgentsFactory
 from environment.backlog_env import BacklogEnv
+from environment.environment import ProductOwnerEnv
 from environment.reward_sytem import BaseRewardSystem
 from pipeline.base_study import BaseStudy
-from algorithms.deep_q_networks import DQN
-from environment.environment import ProductOwnerEnv
-import unittest
 
 
-class TestPipeline(unittest.TestCase):
-    def setUp(self):
+class TestPipeline:
+    def setup_method(self):
         backlog_env = BacklogEnv(sprint_tech_debt_count=0, sprint_commons_count=0, sprint_bugs_count=0)
         reward_system = BaseRewardSystem(config={})
         self.env = ProductOwnerEnv(backlog_env=backlog_env, reward_system=reward_system,
@@ -15,7 +14,8 @@ class TestPipeline(unittest.TestCase):
 
         state_dim = self.env.state_dim
         action_n = self.env.action_n
-        self.agent = DQN(state_dim, action_n)
+        agent_factory = DqnAgentsFactory()
+        self.agent = agent_factory.create_dqn(state_dim, action_n)
 
         self.study = BaseStudy(self.env, self.agent, 10)
 
