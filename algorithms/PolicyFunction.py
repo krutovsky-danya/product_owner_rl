4dc949b108a6aab4df4513fb19f6969c17fd3b2f
krutovsky-danya <krutovsky.danya@gmail.com>
2025-04-11 22:52:05 +0300

    Использовал PolicyFunction

---
+++ algorithms/PolicyFunction.py
@@ -0,0 +1,43 @@
+from algorithms.base_neural_function import BaseNeuralFunction
+
+
+import torch
+from torch import nn
+
+
+class PolicyFunction(BaseNeuralFunction):
+    def __init__(self, state_dim, action_n, inner_layer=256):
+        super().__init__(state_dim, action_n, inner_layer)
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, states):
+        logits = super().forward(states)
+        return self.softmax(logits)
+
+    @torch.no_grad()
+    def predict(self, state):
+        logits = super().predict(state)
+        return self.softmax(logits)
+
+    def forward_guided(self, states, guides) -> torch.Tensor:
+        logits: torch.Tensor = super().forward(states)
+        # masked_fill is out-of-place version of masked_fill_
+        # (in code it usually looks like this: tensor[mask] = value
+        # where mask is a Tensor of booleans)
+        logits = logits.masked_fill(~guides, -torch.inf)
+        return self.softmax(logits)
+
+    def get_normalized_guided_logits(self, states, guides) -> torch.Tensor:
+        """Returns normalized logits for guided actions."""
+        logits: torch.Tensor = super().forward(states)
+        logits = logits.masked_fill(~guides, -torch.inf)
+        logits = logits - torch.logsumexp(logits, dim=1, keepdim=True)
+        return logits
+
+    @torch.no_grad()
+    def predict_guided(self, state, guide):
+        logits = super().predict(state)
+        guide = guide.unsqueeze(dim=0)
+        logits[~guide] = -torch.inf
+        return self.softmax(logits)
--- algorithms/soft_actor_critic.py
+++ algorithms/soft_actor_critic.py
@@ -3,45 +3,8 @@ from torch import nn
 import numpy as np
 import random
 
-from .base_neural_function import BaseNeuralFunction
+from .PolicyFunction import PolicyFunction
 from .q_function import QFunction
-
-
-class PolicyFunction(BaseNeuralFunction):
-    def __init__(self, state_dim, action_n, inner_layer=256):
-        super().__init__(state_dim, action_n, inner_layer)
-
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, states):
-        logits = super().forward(states)
-        return self.softmax(logits)
-
-    @torch.no_grad()
-    def predict(self, state):
-        logits = super().predict(state)
-        return self.softmax(logits)
-
-    def forward_guided(self, states, guides):
-        logits: torch.Tensor = super().forward(states)
-        # masked_fill is out-of-place version of masked_fill_
-        # (in code it usually looks like this: tensor[mask] = value
-        # where mask is a Tensor of booleans)
-        logits = logits.masked_fill(~guides, -torch.inf)
-        return self.softmax(logits)
-
-    def get_normalized_guided_logits(self, states, guides):
-        logits: torch.Tensor = super().forward(states)
-        logits = logits.masked_fill(~guides, -torch.inf)
-        logits = logits - torch.logsumexp(logits, dim=1, keepdim=True)
-        return logits
-
-    @torch.no_grad()
-    def predict_guided(self, state, guide):
-        logits = super().predict(state)
-        guide = guide.unsqueeze(dim=0)
-        logits[~guide] = -torch.inf
-        return self.softmax(logits)
 
 
 def _take_optimization_step(optimizer: torch.optim.Optimizer, loss: torch.Tensor):
