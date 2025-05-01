b816920909a44186cf335f88c5c0461ab29f4225
krutovsky-danya <krutovsky.danya@gmail.com>
2025-05-01 16:07:18 +0300

    Refactor training function to accept parameters for episode count and trajectory length; update embedding sizes for experiments

--- experiments/neural_net_size/training.py
+++ experiments/neural_net_size/training.py
@@ -1,10 +1,8 @@
 import datetime
 import sys
-
 import torch
 
-sys.path.append("..")
-sys.path.append("../..")
+sys.path.extend(["..", "../.."])
 
 from algorithms import DQN
 from algorithms.agents_factory import DqnAgentsFactory
@@ -14,10 +12,8 @@ from pipeline import LoggingStudy
 from training_utils import save_evaluation, save_study_data, make_evaluations
 
 
-def train(agents_factory: DqnAgentsFactory, env_factory):
-    episode_n = 1500
-    trajectory_max_len = 1000
-
+def train(agents_factory: DqnAgentsFactory, env_factory, episode_n=1500, trajectory_max_len=1000):
+    """Train a DQN agent in the given environment."""
     device = "CUDA" if torch.cuda.is_available() else "CPU"
 
     env: ProductOwnerEnv = env_factory()
@@ -26,22 +22,28 @@ def train(agents_factory: DqnAgentsFactory, env_factory):
 
     study.study_agent(episode_n)
 
-    embeding_size = agents_factory.q_function_embeding_size
-    experiment_name = agent.__class__.__name__ + "_" + str(embeding_size) + "_" + device
+    experiment_name = f"{agent.__class__.__name__}_{agents_factory.q_function_embeding_size}_{device}"
     print(f"Experiment name: {experiment_name}")
 
     save_study_data(study, experiment_name)
-
     evaluations = make_evaluations(study, 1000)
 
     now = datetime.datetime.now()
     save_evaluation(experiment_name, evaluations, now, experiment_name)
 
 
-if __name__ == "__main__":
+def main():
+    """Main function to run training experiments."""
     environment_factory = EnvironmentFactory()
-    embeding_sizes = [32, 64, 128, 256, 512, 1024]
-    for embeding_size in [256]:
+    embeding_sizes = [8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+    num_experiments = 5
+
+    for embeding_size in embeding_sizes:
         agents_factory = DqnAgentsFactory(q_function_embeding_size=embeding_size)
-        for i in range(5):
+        for i in range(num_experiments):
+            print(f"Running experiment {i + 1}/{num_experiments} for embedding size {embeding_size}")
             train(agents_factory, environment_factory.create_full_env)
+
+
+if __name__ == "__main__":
+    main()
