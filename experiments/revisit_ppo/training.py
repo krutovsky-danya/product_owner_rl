079e389f0b432ad0ad71b71bfab77b5d65683558
krutovsky-danya <krutovsky.danya@gmail.com>
2025-05-03 20:52:38 +0300

    Increase trajectory count and epochs for improved training in main function

--- experiments/revisit_ppo/training.py
+++ experiments/revisit_ppo/training.py
@@ -29,9 +29,9 @@ def make_credit_study(
 
 
 def main(agent_class):
-    episode_n = 1000
+    episode_n = 2000
     trajectory_max_len = 1000
-    trajectory_n = 1
+    trajectory_n = 5
     study = make_credit_study(trajectory_max_len, episode_n, trajectory_n, agent_class)
     experiment_name = study.agent.__class__.__name__
     save_study_data(study, experiment_name)
@@ -46,10 +46,10 @@ def main(agent_class):
 
 
 if __name__ == "__main__":
-    n = 1
+    n = 5
     agent_factory = PPOAgentsFactory()
-    agent_factory.epoch_n = 1
+    agent_factory.epoch_n = 5
+    agent_factory.inner_layer = 2048
     for i in range(n):
         main(agent_factory.create_ppo_discrete_logits_guided)
-        main(agent_factory.create_ppo_discrete_logits_guided_advantage)
         print(f"Iteration {i + 1} completed.")
